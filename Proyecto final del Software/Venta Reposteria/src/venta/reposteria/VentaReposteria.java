e83b0e5df9f5520817a8de87eb558b20a253e7f8
MoisesLopezVan <figueroalpezverificacion@gmail.com>
2020-06-30 22:50:12 -0500

    Subiendo proyecto

---
+++ Proyecto final del Software/Venta Reposteria/src/venta/reposteria/VentaReposteria.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package venta.reposteria;
+
+/**
+ *
+ * @author Moises Dario
+ */
+public class VentaReposteria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        DiseñoPasteles menu = new DiseñoPasteles(); 
+        menu.setVisible(true);
+    }
+    
+}
---
+++ ReposteriaGlobito/src/Reposteria/Pasteleria.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Reposteria;
+
+/**
+ *
+ * @author crash
+ */
+public class Pasteleria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cuenta cuenta = new Cuenta ();
+        cuenta.setVisible (true);
+        cuenta.setResizable (false);
+        cuenta.setLocationRelativeTo (null);
+        cuenta.setTitle ("Iniciar Sesión");
+    }
+    
+}
